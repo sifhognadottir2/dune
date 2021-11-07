4c29d2ab7ef90ad11f79a57b4611d778e92b081f
Sif Hognadottir <sif.hognadottir@gmail.com>
2021-11-07 12:59:57 +0100

    Maneuver/ExpandingSquare: Waypoint calucation and execution monitoring

--- src/Maneuver/ExpandingSquare/Task.cpp
+++ src/Maneuver/ExpandingSquare/Task.cpp
@@ -29,6 +29,12 @@
 
 // DUNE headers.
 #include <DUNE/DUNE.hpp>
+#include<cmath>
+#include <DUNE/Coordinates/WGS84.hpp>
+#include <DUNE/Math/Angles.hpp>
+
+using namespace DUNE::Math;
+using namespace DUNE::Coordinates;
 
 namespace Maneuver
 {
@@ -40,14 +46,137 @@ namespace Maneuver
   {
     using DUNE_NAMESPACES;
 
-    struct Task: public DUNE::Tasks::Task
+    struct Task: public DUNE::Maneuvers::Maneuver
     {
+      //! Maneuver 
+      IMC::ExpandingSquare m_maneuver;
+      //! EstimatedState
+      //IMC::EstimatedState m_state;
+      //! DesiredPath
+      IMC::DesiredPath m_path;
+      //! Waypoints
+      std::vector<int> m_waypoints_lon;
+      std::vector<int> m_waypoints_lat;
+      //! hstep
+      unsigned int m_hstep;
+      //! Waypoint Counter
+      int m_waypoint_count; 
+      
       //! Constructor.
       //! @param[in] name task name.
       //! @param[in] ctx context.
       Task(const std::string& name, Tasks::Context& ctx):
-        DUNE::Tasks::Task(name, ctx)
+        DUNE::Maneuvers::Maneuver(name, ctx)
       {
+        bindToManeuver<Task, IMC::ExpandingSquare>();
+        
+      }
+
+      // For cleanp or reset local helper variables
+      void onManeuverDeactivation(void) {
+
+      }
+
+      void consume(const IMC::ExpandingSquare* maneuver) {
+        if (maneuver->getSource() != getSystemId())
+          return;
+
+        m_maneuver = *maneuver;
+        m_hstep = maneuver->hstep;
+        double x_first_pair = 0;
+        double y_first_pair = 0;
+        double x_second_pair = 0;
+        double y_second_pair = 0;
+        double x_prev = 0;
+        double y_prev = 0;
+        uint8_t iteration = 0;
+        uint8_t select_x_dir;
+        uint8_t select_y_dir;
+
+        // Calculates two waypoint pairs (x,y) at each iteration
+        while (x_prev <= maneuver->width/2 && y_prev <= maneuver->width/2) {
+          if (maneuver->FLG_CURVE_RIGHT){
+            select_x_dir = pow(-1,iteration);
+            select_y_dir = pow(-1,iteration);
+          } else {
+            select_x_dir = pow(-1,iteration);
+            select_y_dir = pow(-1,iteration+1);
+          }
+          
+          x_first_pair = select_x_dir*iteration*m_hstep + x_prev;
+          y_first_pair = y_prev;
+
+          x_second_pair = x_first_pair;
+          y_second_pair = select_y_dir*iteration*m_hstep + y_prev;
+
+          x_prev = x_second_pair;
+          y_prev = y_second_pair; 
+
+          // Rotate w.r.t. bearing angle
+          Angles::rotate(maneuver->bearing,false,x_first_pair,y_first_pair);
+          Angles::rotate(maneuver->bearing,false,x_second_pair,y_second_pair);
+
+          double *lat_first_pair;// = maneuver->lat;
+          double *lon_first_pair;// = maneuver->lon;
+          double *lat_second_pair;// = maneuver->lat;
+          double *lon_second_pair;// = maneuver->lon;
+          // Add offset
+          Coordinates::WGS84::displace(x_first_pair,y_first_pair,lat_first_pair,lon_first_pair);
+          Coordinates::WGS84::displace(x_second_pair,y_second_pair,lat_second_pair,lon_second_pair);
+
+          m_waypoints_lon.push_back(*lon_first_pair);
+          m_waypoints_lon.push_back(*lon_second_pair);
+
+          m_waypoints_lat.push_back(*lat_first_pair);
+          m_waypoints_lat.push_back(*lat_second_pair);
+        
+        } 
+
+        setControl(IMC::CL_PATH);
+        m_path.speed = maneuver->speed;
+        m_path.speed_units = maneuver->speed_units;
+        m_path.end_z = maneuver->z;
+        m_path.end_z_units = maneuver->z_units;
+
+        m_waypoint_count = 0;
+        sendPath(m_waypoints_lat[0],m_waypoints_lon[0]);
+
+          // Set up local variables
+          // start sending the first DesiredPath IMC message
+        
+          // Calculate ExpandingSquare pattern points
+            // Given maneuver configuration return list of lat/loin points to guide vehicle 
+      }
+
+      void onPathControlState(const IMC::PathControlState* pcs) {
+          // Here we monitor the execution of the command. Check if vehicle is near the desired point
+          // if near: send next point in list (similar to RowsCoverage maneuver)
+        if (IMC::PathControlState::FL_NEAR)
+        {
+          if (m_waypoint_count < m_waypoints_lat.size()) {
+            double lat;
+            double lon;
+            lat = m_waypoints_lat[m_waypoint_count];
+            lon = m_waypoints_lon[m_waypoint_count];
+            m_waypoint_count++;
+
+          } else {
+            signalCompletion();
+            return;
+          }
+        }
+      }
+      //! Send new desired path
+      //! @param[in] lat latitude for new desired path
+      //! @param[in] lon longitude for new desired path
+      void
+      sendPath(double lat, double lon)
+      {
+        // Calculate WGS-84 coordinates and fill DesiredPath message
+        m_path.end_lat = lat;
+        m_path.end_lon = lon;
+        m_path.flags = 0;
+        dispatch(m_path);
       }
 
       //! Update internal state with new parameter values.
